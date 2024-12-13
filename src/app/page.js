c2aab37d16c201c9b791c4d18c6bb87e7e166fdd
clarisse.larese <clarisse.larese@yahoo.fr>
2024-12-13 10:32:40 +0100

    About, footer and form added in Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,27 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // Importe les styles de FontAwesome
+config.autoAddCss = false; // Désactive l'ajout automatique du CSS par FontAwesome
+
+import About from "@/components/about/About";
+import Footer from "@/components/footer/Footer";
 import Hero from "@/components/hero/Hero";
 import Navbar from "@/components/navbar/Navbar";
 import Preheader from "@/components/preheader/Preheader";
 import Services from "@/components/servicesSection/Services";
+import Form from "@/components/form/Form";
+// import StickyNavbar from "@/components/stickyNavbar/StickyNavbar";
 
 export default function Home() {
   return (
    <>
     <Preheader />
     <Navbar />
+    {/* <StickyNavbar /> */}
     <Hero />
     <Services />
+    <About />
+    <Form />
+    <Footer />
    </>
   );
 }
